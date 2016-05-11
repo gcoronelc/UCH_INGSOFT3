141c94fc6e0eef58e1e81060ece4afb9b893ddf5
201-MODULO <201-MODULO>
2016-05-11 21:42:04 +0200

    Clase 10

--- Sem10/AprendiendoJDBC/src/java/pe/egcc/service/EurekaService.java
+++ Sem10/AprendiendoJDBC/src/java/pe/egcc/service/EurekaService.java
@@ -1,6 +1,8 @@
 package pe.egcc.service;
 
+import java.util.List;
 import pe.egcc.dao.EurekaDao;
+import pe.egcc.domain.Cliente;
 
 
 public class EurekaService {
@@ -14,5 +16,9 @@ public class EurekaService {
   
   public void addParametro(String codigo, String descripcion, String valor){
     dao.addParametro(codigo, descripcion, valor);
+  }
+
+  public List<Cliente> getClientes(Cliente cliente) {
+    return dao.getClientes(cliente);
   }
 }
--- Sem10/AprendiendoJDBC/src/java/pe/egcc/servlets/EurekaController.java
+++ Sem10/AprendiendoJDBC/src/java/pe/egcc/servlets/EurekaController.java
@@ -1,15 +1,17 @@
 package pe.egcc.servlets;
 
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import pe.egcc.domain.Cliente;
 import pe.egcc.service.EurekaService;
 
-@WebServlet({"/AddParametro"})
+@WebServlet({"/AddParametro", "/ClienteConsultar"})
 public class EurekaController extends HttpServlet {
 
   private EurekaService es;
@@ -24,6 +26,8 @@ public class EurekaController extends HttpServlet {
     String path = request.getServletPath();
     if (path.equals("/AddParametro")) {
       addParametro(request, response);
+    } else if (path.equals("/ClienteConsultar")) {
+      clienteConsultar(request, response);
     }
 
   }
@@ -45,6 +49,29 @@ public class EurekaController extends HttpServlet {
     // Forward
     RequestDispatcher rd;
     rd = request.getRequestDispatcher("demo001.jsp");
+    rd.forward(request, response);
+  }
+
+  private void clienteConsultar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try {
+      // Datos
+      Cliente cliente = new Cliente();
+      cliente.setCodigo(request.getParameter("codigo"));
+      cliente.setPaterno(request.getParameter("paterno"));
+      cliente.setMaterno(request.getParameter("materno"));
+      cliente.setNombre(request.getParameter("nombre"));
+      // Proceso
+      List<Cliente> lista = es.getClientes(cliente);
+      // Salida
+      request.setAttribute("lista", lista);
+      request.setAttribute("bean", cliente);
+    } catch (Exception e) {
+      request.setAttribute("error", e.getMessage());
+    }
+
+    // Forward
+    RequestDispatcher rd;
+    rd = request.getRequestDispatcher("demo002.jsp");
     rd.forward(request, response);
   }
 
