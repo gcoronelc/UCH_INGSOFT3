bcd9422c73cf5962b9d52d787805f065d4628e9f
201-MODULO <201-MODULO>
2016-05-25 21:01:35 +0200

    Practica

--- Sem12/ParaLaPractica.sql
+++ Sem12/ParaLaPractica.sql
@@ -23,6 +23,8 @@ vch_tipoaccion
 vch_tipoestado
 
 
+-- PREGUNTA 1
+
 select 
 	me.vch_monedescripcion moneda,
 	sum(case when tm.vch_tipoaccion='INGRESO' 
@@ -36,6 +38,53 @@ join cuenta cu on me.chr_monecodigo = cu.chr_monecodigo
 join movimiento mv on cu.chr_cuencodigo = mv.chr_cuencodigo
 join tipomovimiento tm on mv.chr_tipocodigo = tm.chr_tipocodigo
 group by me.vch_monedescripcion;
+
+
+
+-- PREGUNTA 2
+
+
+select 
+    cu.chr_cuencodigo cuenta,
+	me.vch_monedescripcion moneda,
+	sum(case when tm.vch_tipoaccion='INGRESO' 
+		then dec_moviimporte else 0 end) ingresos,
+	sum(case when tm.vch_tipoaccion='SALIDA' 
+		then dec_moviimporte else 0 end) salida,
+	sum(dec_moviimporte * case when tm.vch_tipoaccion='SALIDA' 
+		then -1 else 1 end) saldo
+from moneda me
+join cuenta cu on me.chr_monecodigo = cu.chr_monecodigo
+join movimiento mv on cu.chr_cuencodigo = mv.chr_cuencodigo
+join tipomovimiento tm on mv.chr_tipocodigo = tm.chr_tipocodigo
+where cu.chr_sucucodigo = '001'
+group by cu.chr_cuencodigo, me.vch_monedescripcion;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
